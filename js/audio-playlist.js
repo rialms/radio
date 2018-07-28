2744a43843babb164b5da58665ae2566d323ddea
rialms <checkoutrons@gmail.com>
2018-07-28 06:15:49 +0530

    Add files via upload

---
+++ js/audio-playlist.js
@@ -0,0 +1,260 @@
+//<![CDATA[
+$(document).ready(function(){
+    "use strict";
+    new jPlayerPlaylist({
+        jPlayer: "#musica-jquery-jplayer",
+        cssSelectorAncestor: "#musica-audio-player-container"
+    }, [
+        
+
+		
+{ 
+            title:"RADIO FOORTI ", 
+            artist:"<span class='music-time'>88.0 FM</span>",
+            mp3:"http://119.148.23.88:1021/;",
+            oga:"http://119.148.23.88:1021/;",
+            poster: "images/home/poster1.jpg",
+			
+        },
+	 {
+            title:"Radio Swadesh",
+            artist:"<span class='music-time'>RadioSwadesh.NET</span>",
+            mp3:"http://radioswadesh.com:8002/stream/",
+            oga:"http://radioswadesh.com:8002/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        }, 
+
+	 {
+            title:"Radio Shongi",
+            artist:"<span class='music-time'>RadioShongi.com</span>",
+            mp3:"http://162.254.150.34:7031/;",
+            oga:"http://162.254.150.34:7031/;",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Radio Kotha",
+            artist:"<span class='music-time'>RadioKotha.com</span>",
+            mp3:"http://188.165.201.120:8016/stream",
+            oga:"http://188.165.201.120:8016/stream",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Radio Wadio",
+            artist:"<span class='music-time'>BanglaWadio.com/span>",
+            mp3:"http://162.254.150.34:8201/stream/",
+            oga:"http://162.254.150.34:8201/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"RobicChaya",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8032/stream/",
+            oga:"http://radio.bongonet.net:8032/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Chirantanee",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8036/stream/",
+            oga:"http://radio.bongonet.net:8036/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Mono Phone",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8040/stream/",
+            oga:"http://radio.bongonet.net:8040/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"DiscoBani Kolkata",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8008/stream/",
+            oga:"http://radio.bongonet.net:8008/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+
+	 {
+            title:"Radio BanglaNET",
+            artist:"<span class='music-time'>RadioBanglaNET.com/</span>",
+            mp3:"http://sc6.spacialnet.com:33656/;",
+            oga:"http://sc6.spacialnet.com:33656/;",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Radio Goon Goon",
+            artist:"<span class='music-time'>RadioGoonGooN.com</span>",
+            mp3:"http://live.radiogoongoon.com:8888/stream/",
+            oga:"http://live.radiogoongoon.com:8888/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+
+	 {
+            title:"FNF.FM",
+            artist:"<span class='music-time'>FnF.FM</span>",
+            mp3:"http://eu6.fastcast4u.com:5099/;stream",
+            oga:"http://eu6.fastcast4u.com:5099/;stream",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+
+	 {
+            title:"Radio BongOnet",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8000/stream/",
+            oga:"http://radio.bongonet.net:8000/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"GO Yester Years",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8016/stream/",
+            oga:"http://radio.bongonet.net:8016/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+		 	 {
+            title:"Folk NAMA",
+            artist:"<span class='music-time'>BongoNET.NET</span>",
+            mp3:"http://radio.bongonet.net:8004/stream/",
+            oga:"http://radio.bongonet.net:8004/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+          }, 
+	 {
+            title:"VOA NEWS",
+            artist:"<span class='music-time'>voanews.com</span>",
+            mp3:"https://av.voanews.com/clips/VBA/2018/07/27/20180727-160000-VBA043-program.mp3",
+            oga:"https://av.voanews.com/clips/VBA/2018/07/27/20180727-160000-VBA043-program.mp3",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Radio 71'",
+            artist:"<span class='music-time'>98.4 FM</span>",
+            mp3:"http://103.253.47.173:8000/stream/",
+            oga:"http://103.253.47.173:8000/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+        {
+            title:"Bangladesh Betar",
+            artist:"<span class='music-time'>1503 AM</span>",
+            mp3:"http://live.canstream.co.uk:8000/betarbangla.mp3",
+            oga:"http://live.canstream.co.uk:8000/betarbangla.mp3",
+            poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+        },
+	 {
+            title:"Bangladesh Betar",
+            artist:"<span class='music-time'>102.5 FM</span>",
+            mp3:"http://ample-zeno-14.radiojar.com/8w0533k6vewtv",
+            oga:"http://ample-zeno-14.radiojar.com/8w0533k6vewtv",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	 {
+            title:"Radio Bhumi",
+            artist:"<span class='music-time'>92.8 FM</span>",
+            mp3:"http://149.56.195.94:8545/stream/",
+            oga:"http://149.56.195.94:8545/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+        {
+            title:"Lemon24",
+            artist:"<span class='music-time'>Lemon24.com</span>",
+            mp3:"http://office.mcc.com.bd:8000/;",
+            oga:"http://office.mcc.com.bd:8000/;",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        },
+	 {
+            title:"Colors FM",
+            artist:"<span class='music-time'>101.6 FM</span>",
+            mp3:"http://45.64.135.88:8000/stream/1/",
+            oga:"http://45.64.135.88:8000/stream/1/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },
+	{
+            title:"BBC Bangla",
+            artist:"<span class='music-time'>bbc.com</span>",
+            mp3:"http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-benga_backup",
+            oga:"http://bbcwssc.ic.llnwd.net/stream/bbcwssc_mp1_ws-benga_backup",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        },
+
+
+	  {
+            title:"SpiceFM",
+            artist:"<span class='music-time'>96.4 FM</span>",
+            mp3:"http://162.254.149.187:9300/stream/",
+            oga:"http://162.254.149.187:9300/stream/",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+          },
+	  {
+            title:"Radio Shadhin",
+            artist:"<span class='music-time'>92.4 FM</span>",
+            mp3:"http://ample-zeno-16.radiojar.com/umq9q5uuva5tv",
+            oga:"http://ample-zeno-16.radiojar.com/umq9q5uuva5tv",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },      
+ 	 {
+            title:"DhakaFM",
+            artist:"<span class='music-time'>90.4 FM</span>",
+            mp3:"http://118.179.219.244:8000/;",
+            oga:"http://118.179.219.244:8000/;",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+         },    
+ 	 {
+            title:"Peoples Radio",
+            artist:"<span class='music-time'>91.6 FM</span>",
+            mp3:"http://s3.myradiostream.com:14498/;",
+            oga:"http://s3.myradiostream.com:14498/;",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        },  
+
+        {
+            title:"ABC RADIO",
+            artist:"<span class='music-time'>89.2 FM</span>",
+            mp3:"http://ample-zeno-08.radiojar.com/6a28tbx6vewtv",
+            oga:"http://ample-zeno-08.radiojar.com/6a28tbx6vewtv",
+            poster: "http://www.jplayer.org/audio/poster/The_Stark_Palace_640x360.png"
+        },
+        {
+            title:"RADIO TODAY",
+            artist:"<span class='music-time'>89.6 FM</span>",
+            mp3:"http://ample-zeno-17.radiojar.com/8wv4d8g4344tv",
+            oga:"http://ample-zeno-17.radiojar.com/8wv4d8g4344tv",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        },
+{ 
+            title:"RADIO FOORTI ", 
+            artist:"<span class='music-time'>88.0 FM</span>",
+            mp3:"http://119.148.23.88:1021/;",
+            oga:"http://119.148.23.88:1021/;",
+            poster: "images/home/poster1.jpg",
+			
+        },
+        {
+            title:"অন্যান্য চ্যানেলের জন্য এই ক্রস (X) বাটনে চাপ দাও",
+            artist:"<span class='music-time'></span>",
+            mp3:"#",
+            oga:"#",
+            poster: "http://www.jplayer.org/audio/poster/Miaow_640x360.png"
+        }
+    ], {
+        playlistOptions: {
+            autoPlay: false,
+            loopOnPrevious: true,
+            shuffleOnLoop: true,
+            enableRemoveControls: true,
+            displayTime: "show",
+            freeItemClass: "jp-playlist-item-free",
+        },
+        swfPath: "js/jquery.jplayer.swf",
+        supplied: "oga, mp3",
+        wmode: "window",
+        useStateClassSkin: true,
+        autoBlur: true,
+        smoothPlayBar: true,
+        keyEnabled: true,
+        remainingDuration: true,
+        volume: 1,
+    });
+
+
+
+});
+//]]>
